5fdfa81d37313f9fc030066725aac63b67979ce8
Waffl536 <waffl536@gmail.com>
2024-10-04 17:52:13 +0300

    Added some basic functionality

---
+++ build/CMakeFiles/Stickies++.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Stickies++.dir/src/NoteWindow.cpp.o"
+  "CMakeFiles/Stickies++.dir/src/NoteWindow.cpp.o.d"
+  "CMakeFiles/Stickies++.dir/src/main.cpp.o"
+  "CMakeFiles/Stickies++.dir/src/main.cpp.o.d"
+  "Stickies++"
+  "Stickies++.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Stickies++.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/Stickies++.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Stickies++.
+# This may be replaced when dependencies are built.
